2ea6742c9b2abea82160de8d07079a7c87bf6d6f
HarshChaurasia11 <harsh.chaurasia.dev@gmail.com>
2026-09-21 00:11:08 +0530

    string

---
+++ JS/Day 5 Strings/Index.js
@@ -0,0 +1,32 @@
+// Question 1 — String Creation
+
+var name = "Harsh"
+var course = "Javascript"
+var message = "I am learning Javascript"
+
+console.log(name, course, message)
+
+
+// Question 2 — Find String Length
+
+var word = "JavaScript"
+
+console.log(word.length) 
+
+
+// Question 3 — String Indexing
+
+var word = "JavaScript"
+
+console.log(word[0])
+console.log(word[2])
+console.log(word[9])
+
+
+// Question 4 — Using charAt()
+
+var word = "TypeScript"
+
+console.log(word.charAt(0))
+console.log(word.charAt(2))
+console.log(word.charAt(9))
